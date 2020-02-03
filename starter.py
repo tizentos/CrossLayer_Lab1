ed872cd4ba52686517c82d94a6b9d7dc13b94e42
tizentos <tizentos@gmail.com>
2020-02-03 16:57:38 -0600

    Starter Code

---
+++ starter.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import argparse
+
+# argument parser
+parser = argparse.ArgumentParser(description='ML_CODESIGN Lab1 - MNIST example')
+parser.add_argument('--batch-size', type=int, default=100, help='Number of samples per mini-batch')
+parser.add_argument('--epochs', type=int, default=10, help='Number of epoch to train')
+parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+args = parser.parse_args()
+
+# Hyper Parameters
+input_size = 784
+num_classes = 10
+num_epochs = args.epochs
+batch_size = args.batch_size
+learning_rate = args.lr
+
+# MNIST Dataset (Images and Labels)
+train_dataset = dsets.MNIST(root ='./data',
+        train = True,
+        transform = transforms.ToTensor(),
+        download = True)
+
+test_dataset = dsets.MNIST(root ='./data',
+        train = False,
+        transform = transforms.ToTensor())
+
+# Dataset Loader (Input Pipeline)
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+        batch_size = batch_size,
+        shuffle = True)
+
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+        batch_size = batch_size,
+        shuffle = False)
+
+# Model
+class LogisticRegression(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(input_size, num_classes)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+model = LogisticRegression(input_size, num_classes)
+
+# Loss and Optimizer
+# Softmax is internally computed.
+# Set parameters to be updated.
+criterion = nn.CrossEntropyLoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+
+# Training the Model
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = Variable(images.view(-1, 28 * 28))
+        labels = Variable(labels)
+
+        # Forward + Backward + Optimize
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        # (1)
+        loss.backward()
+        # (2)
+        optimizer.step()
+        # (3)
+
+        if (i + 1) % 100 == 0:
+            print('Epoch: [% d/% d], Step: [% d/% d], Loss: %.4f'
+                    % (epoch + 1, num_epochs, i + 1,
+                       len(train_dataset) // batch_size, loss.data.item()))
+
+# Test the Model
+correct = 0
+total = 0
+for images, labels in test_loader:
+    images = Variable(images.view(-1, 28 * 28))
+    outputs = model(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum()
+
+print('Accuracy of the model on the 10000 test images: % d %%' % (100 * correct / total))
